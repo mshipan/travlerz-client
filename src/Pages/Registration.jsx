5f5eac9ffc6dfc5acf1f697fab2d260589f01ba4
mshipan <mshipan657@gmail.com>
2023-06-30 13:17:21 +0600

    fix registration page responsive design issue

--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -1,21 +1,29 @@
 import { Helmet } from "react-helmet-async";
 import logo from "../assets/travlerz-logo.png";
+import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 const Registration = () => {
   return (
-    <div className="bg-registerBg w-full h-screen bg-no-repeat bg-cover bg-center flex items-center justify-center">
+    <div className="bg-registerBg w-full h-fit md:h-screen bg-no-repeat bg-cover bg-center flex items-center justify-center">
       <Helmet>
         <title>Registration | Travlerz</title>
       </Helmet>
-      <div className="bg-white bg-opacity-40 w-[45rem] h-[40rem] md:h-[31rem] rounded-lg shadow-2xl">
+      <div className="bg-white bg-opacity-40 md:w-1/2 md:h-2/3 p-1 m-5 rounded-lg shadow-2xl flex flex-col items-center justify-center">
         <div className="flex items-center justify-center my-1">
           <img src={logo} alt="Website Logo" className="w-48" />
         </div>
         <h1 className="text-2xl font-barlow font-bold mb-3 md:mb-5 text-center">
-          Sign Up Now
+          Sign Up with
         </h1>
-        <div className="w-3/4 md:w-full mx-auto">
-          <form className="flex flex-col gap-5 md:gap-8 px-5">
+        <div>
+          <button className="flex items-center gap-1 text-lg bg-[#131D4E] text-white px-5 py-1">
+            <FcGoogle className="text-2xl" /> Google
+          </button>
+        </div>
+        <div className="divider w-1/2 mx-auto text-xl font-barlow my-5">OR</div>
+        <div className="w-full mx-auto">
+          <form className="flex flex-col gap-3 md:gap-8 px-5">
             <div className="flex flex-col md:flex-row items-center justify-center md:gap-8 w-full ">
               <div className="w-full md:w-1/2 flex flex-col md:gap-3">
                 <div className="form-control">
@@ -26,7 +34,7 @@ const Registration = () => {
                     type="text"
                     name="name"
                     placeholder=" Type Your Full Name"
-                    className="p-1 focus:outline-none border border-[#131D4E] "
+                    className="p-1 focus:outline-none border border-[#131D4E] placeholder:font-mono"
                   />
                 </div>
                 <div className="form-control">
@@ -37,7 +45,7 @@ const Registration = () => {
                     type="email"
                     name="email"
                     placeholder=" Type Your Email"
-                    className="p-1 focus:outline-none border border-[#131D4E] "
+                    className="p-1 focus:outline-none border border-[#131D4E] placeholder:font-mono"
                   />
                 </div>
                 <div className="form-control">
@@ -51,7 +59,7 @@ const Registration = () => {
                     type="password"
                     name="password"
                     placeholder=" Type Your Password"
-                    className="p-1 focus:outline-none border border-[#131D4E] "
+                    className="p-1 focus:outline-none border border-[#131D4E] placeholder:font-mono"
                   />
                 </div>
                 <div className="form-control">
@@ -65,7 +73,7 @@ const Registration = () => {
                     type="password"
                     name="confirmPassword"
                     placeholder=" Confirm Password"
-                    className="p-1 focus:outline-none border border-[#131D4E] "
+                    className="p-1 focus:outline-none border border-[#131D4E] placeholder:font-mono"
                   />
                 </div>
               </div>
@@ -105,7 +113,7 @@ const Registration = () => {
                     type="text"
                     name="country"
                     placeholder=" Type Your Country"
-                    className="p-1 focus:outline-none border border-[#131D4E] "
+                    className="p-1 focus:outline-none border border-[#131D4E] placeholder:font-mono"
                   />
                 </div>
                 <div className="form-control">
@@ -116,7 +124,7 @@ const Registration = () => {
                     type="text"
                     name="phone"
                     placeholder=" Your Phone Number"
-                    className="p-1 focus:outline-none border border-[#131D4E] "
+                    className="p-1 focus:outline-none border border-[#131D4E] placeholder:font-mono"
                   />
                 </div>
               </div>
@@ -127,6 +135,17 @@ const Registration = () => {
                 value="Sign Up"
                 className="px-5 py-2 border border-[#131D4E] bg-[#131D4E] hover:bg-white text-white hover:text-[#131D4E] duration-500 font-barlow font-bold cursor-pointer"
               />
+            </div>
+            <div className="flex items-center justify-center mb-1">
+              <p className="font-barlow md:text-lg font-semibold">
+                Already have an account?{" "}
+                <span className="italic bg-blue-500 px-1 text-white">
+                  Please{" "}
+                  <Link to="/login" className="underline hover:no-underline">
+                    Login.
+                  </Link>
+                </span>
+              </p>
             </div>
           </form>
         </div>
